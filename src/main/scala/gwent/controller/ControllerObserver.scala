ad7258e576a97a915a269bd648316d715acc8ba2
Elemma00 <faundez76@gmail.com>
2023-07-06 18:30:03 -0400

    se ha usado el patron observer para notificar que gano un jugador

---
+++ src/main/scala/gwent/controller/ControllerObserver.scala
@@ -0,0 +1,7 @@
+package cl.uchile.dcc
+package gwent.controller
+
+trait Observer {
+
+  def update()
+}
---
+++ src/main/test/cl/uchile/dcc/test/NotifyTest.scala
@@ -0,0 +1,7 @@
+package cl.uchile.dcc
+package test
+
+class StateTest extends munit.FunSuite {
+  
+  
+}
